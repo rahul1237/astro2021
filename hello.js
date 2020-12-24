c246440523932eafb3b502adb6689df4d0bf39a0
Rahul Mahajan <rahulmahajan403@outlook.com>
2020-12-25 01:26:04 +0530

    :/

--- hello.js
+++ hello.js
@@ -1,19 +1,29 @@
 const validator = require("validator")  //this is the package to check the string!
 const chalk = require("chalk")  //this is the package to use diff colors in the console!
+const yargs = require("yargs")
+
+
+//                 yargs this is the package that is also used for taking input from
+//                      the users! ---> (yargs.argv)
+
+console.log(yargs.argv);
+
+
+
 
 
 //                  process.argv -> this is used to take the input from the user!
 //                          argv is the vector that contains the user input on the
 //                          2 position wrt to its index position!
 
-const user_input = process.argv[2]
-if(user_input=='add'){
-    console.log('data successfully added into the todo list!');
-}else if(user_input=='delete'){
-    console.log('data successfully deleted from the todo list!');
-}else{
-    console.log('AHAA! invalid input! :/');
-}
+// const user_input = process.argv[2]
+// if(user_input=='add'){
+//     console.log('data successfully added into the todo list!');
+// }else if(user_input=='delete'){
+//     console.log('data successfully deleted from the todo list!');
+// }else{
+//     console.log('AHAA! invalid input! :/');
+// }
 
 
 
